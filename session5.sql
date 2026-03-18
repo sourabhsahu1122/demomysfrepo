d04c68b03dad163840f933b88aa274268642468a
RAM <ramkrishna20261@outlook.com>
2026-03-18 02:38:10 +0000

    my second commit

--- session5.sql
+++ session5.sql
@@ -1,12 +1,5 @@
 CREATE Table session5.session5sch.Customers(customer_id int, name String, email String);
 
-insert into session5.session5sch.Customers (customer_id,name,email)
-values(1,'Sourabh','s3@s.com')
 
-insert into session5.session5sch.Customers (customer_id,name,email)
-values(2,'Sourabh','s3@s.com')
-
-insert into session5.session5sch.Customers (customer_id,name,email)
-values(3,'Sourabh','s3@s.com')
 
 select * from session5.session5sch.customers;
